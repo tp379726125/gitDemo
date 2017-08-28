da210e13c87a8478dca2f291ac7644f25c188bc7
tp379726125 <18225404148@163.com>
2017-08-28 20:10:00 +0800

    xiangm

---
+++ Demo/Demo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Demo
+//
+//  Created by 谭攀 on 2017/8/28.
+//  Copyright © 2017年 TanPan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
